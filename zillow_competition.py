23f8d0917e837165b8d608bfaa6d7103d5d8e0d2
caspHa <caspar.hasner@gmail.com>
2017-09-15 14:45:51 +0200

    started first model

---
+++ zillow_competition.py
@@ -0,0 +1,20 @@
+# The available files are:
+#
+# zillow_data_dictionary
+# sample_submission
+# properties_2016.csv/properties_2016
+# train_2016.csv/train_2016
+#
+
+
+# import libraries
+import numpy as np
+import pandas as pd
+
+# define custom functions
+
+
+# import data
+properties = pd.read_csv("properties_2016.csv/properties_2016.csv")
+transactions =  
+
